db36c6e7ccf0a06c3ca7029faeea54a9a159c028
Taneros <fricentaurus@GMAIL.COM>
2021-09-10 22:50:57 +0300

    fix:  fix hamburger menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,12 @@
 const hamburger = document.getElementById('hamburger')
+const hamburgerI = document.getElementById('hamburger-i')
+
 const navUl = document.getElementById('nav-ul')
 const headerMenu = document.getElementById('header-menu')
 
 hamburger.addEventListener('click', () => {
   navUl.classList.toggle('show')
   headerMenu.classList.toggle('show')
+  hamburgerI.classList.toggle('fa-bars')
+  hamburgerI.classList.toggle('fa-times')
 })
