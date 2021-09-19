c485f64914408c95ddb0e9b44a3e847edc170a43
Taneros <fricentaurus@GMAIL.COM>
2021-09-19 17:22:09 +0300

    feat: finish styles add video

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,72 @@
 // hljs.highlightAll()
 
-const hamburger = document.getElementById('hamburger')
-const hamburgerI = document.getElementById('hamburger-i')
+const hamburger = document.getElementById("hamburger");
+const hamburgerI = document.getElementById("hamburger-i");
 
-const navUl = document.getElementById('nav-ul')
-const headerMenu = document.getElementById('header-menu')
+const headerMenu = document.getElementById("header-menu");
+const navUl = document.getElementById("nav-ul");
 
-hamburger.addEventListener('click', () => {
-  navUl.classList.toggle('show')
-  headerMenu.classList.toggle('show')
-  hamburgerI.classList.toggle('fa-bars')
-  hamburgerI.classList.toggle('fa-times')
-})
+hamburger.addEventListener("click", () => {
+  navUl.classList.toggle("show");
+  headerMenu.classList.toggle("show");
+  hamburgerI.classList.toggle("fa-bars");
+  hamburgerI.classList.toggle("fa-times");
+});
+
+headerMenu.addEventListener("click", () => {
+  navUl.classList.toggle("show");
+  headerMenu.classList.toggle("show");
+  hamburgerI.classList.toggle("fa-bars");
+  hamburgerI.classList.toggle("fa-times");
+});
+
+selectors = [
+  "article",
+  "aside",
+  "details",
+  "figcaption",
+  "figure",
+  "footer",
+  "header",
+  "main",
+  "mark",
+  "nav",
+  "section",
+  "summary",
+  "time",
+  "h1",
+  "h2",
+  "h3",
+  "h4",
+  "h5",
+  "h6",
+];
+let total = 0;
+selectors.forEach((selector) => {
+  const count = Array.from(document.querySelectorAll(selector)).length;
+  console.log(`${selector}:  ${count}`);
+  if (count > 0) total++;
+});
+
+console.log("Total unique: ", total);
+
+console.log(`
+Самооценка оценка - 160 баллов 
+Выполненные пункты:
+1) вёрстка валидная. Проверить валидность вёрстки: https://validator.w3.org/ 
+2) вёрстка семантическая. В коде страницы присутствуют семантические теги HTML5, например, article, aside, figure, figcaption, footer, header, main, nav, section, используются заголовки h1-h6. 2 балла за каждый уникальный семантический тег HTML5 и за каждый уникальный заголовок h1-h6, но не больше 20 баллов. Заголовок h1 может быть только один 
+3) для оформления СV используются css-стили. 1 балл за каждый стилизованный элемент, но не больше 10 баллов 
+4) контент размещается в блоке, который горизонтально центрируется на странице. Фоновый цвет, если он есть, тянется во всю ширину страницы 
+5) вёрстка адаптивная: ни на одном из разрешений экрана до 320px включительно не появляется горизонтальная полоса прокрутки, при этом всё содержание страницы сохраняется 
+6) есть адаптивное бургер-меню. Ссылки в пунктах меню ведут на основные разделы CV. При кликах по пунктам меню реализована плавная прокрутка по якорям. При уменьшении ширины экрана меню становится адаптивным. 
+7) на странице СV присутствует изображение - фото или аватарка автора CV, пропорции изображения не искажены, у изображения есть атрибут alt (может быть пустым) 
+8) контакты для связи и перечень навыков оформлены в виде списка ul > li 
+9) CV содержит контакты, краткую информацию о себе, навыки, образование, уровень английского 
+10) CV содержит пример вашего кода (например, решение задачи с сайта codewars) с подсветкой кода 
+11) CV содержит изображения-ссылки на выполненные вами проекты. При клике по изображению страница проекта открывается в новой вкладке. У каждого проекта есть название, небольшое описание, указан перечень используемых технологий 
+12) CV выполнено на английском языке 
+13) выполнены требования к репозиторию: есть ссылка на задание, скриншот страницы СV, ссылка на деплой страницы CV на GitHub Pages, указана дата дедлайна, выполнена самооценка (самооценку расписываем по пунктам критериев оценки, указывая балл за каждый пункт) 
+14) есть видеорезюме автора CV на английском языке. Видеорезюме встраивается в страницу CV как видео, а не в виде кнопки или ссылки. Продолжительность видео 3-5 минут (±15 секунд). В описание видео на YouTube добавлена ссылка на его транскрипцию на английском языке (например, в документе Google Docs). 
+15) дизайн, оформление, качество выполнения CV не ниже чем в примерах CV, приведённых в материалах к заданию 
+
+`);
